dbb25447e7efd2632749f75afa073ae1c72a4010
Sumikumar2007 <tsumitkumar419@gmail.com>
2026-04-28 23:55:06 +0530

    Time: 1 ms (94.33%) | Memory: 42.7 MB (49.73%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,22 +1,9 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if(dividend==0){
-            return 0;
-        }
-        else if(divisor==0){
-            return 0 ;
-        }
-        else if(divisor==1){
-            return dividend;
-        }
-        else if(dividend==divisor){
-            return 1;
-        }
-        else if(dividend==Integer.MIN_VALUE && divisor==-1){
+        if (divisor==-1 & dividend==Integer.MIN_VALUE){
             return Integer.MAX_VALUE;
+
         }
-        else{
-            return   dividend/divisor;
-        }
+        return dividend/divisor;
     }
 }
