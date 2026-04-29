7f967b1e68e4fab600614e7e71c9916a34695fb8
Sumikumar2007 <tsumitkumar419@gmail.com>
2026-04-29 23:50:42 +0530

    Time: 1 ms (99.49%) | Memory: 42.5 MB (52.23%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,8 +1,19 @@
 class Solution {
-    public boolean isUgly(int num) {
-        for (int i=2; i<6 && num>0; i++)
-    while (num % i == 0)
-        num /= i;
-    return num == 1;
+    public boolean isUgly(int n) {
+        if (n <= 0){
+            return false;
+        }
+        int[] factors = {2,3,5};
+
+        for(int f:factors){
+           while(n % f == 0){
+                n = n/f;
+            }
+        }
+        if(n == 1){
+            return true;
+        }
+        return false;
+
     }
 }
